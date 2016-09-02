d503120793c513aa765860da95c3ac4c08015e9e
Daniel Friedman <dfriedman58@gmail.com>
2016-09-02 10:42:40 -0400

    Add sourceConfig spec

--- src/sourceConfig.js
+++ src/sourceConfig.js
@@ -1,3 +1,10 @@
+/**
+ * Given a Solid WebID, returns the sourceConfig for modelld fields.  `webId`
+ * must be a solid WebID.
+ *
+ * @param {String} webId - the Solid WebID for a given user.
+ * @returns {Object} - the modelld sourceConfig object.
+ */
 export default function sourceConfig (webId) {
   const baseUrl = webId.replace('/profile/card#me', '')
   const profileURI = `${baseUrl}/profile/card`
---
+++ test/sourceConfig.spec.js
@@ -0,0 +1,21 @@
+import sourceConfig from '../src/sourceConfig'
+
+describe('sourceConfig.js', () => {
+  describe('sourceConfig', () => {
+    const webId = 'https://example.com/profile/card#me'
+
+    it('returns the profile URI as the default and only listed URI', () => {
+      expect(sourceConfig(webId).defaultSources.listed)
+        .toEqual('https://example.com/profile/card')
+      expect(sourceConfig(webId).sourceIndex['https://example.com/profile/card'])
+        .toBe(true)
+    })
+
+    it('returns the preferences URI as the default and only unlisted URI', () => {
+      expect(sourceConfig(webId).defaultSources.unlisted)
+        .toEqual('https://example.com/settings/prefs.ttl')
+      expect(sourceConfig(webId).sourceIndex['https://example.com/settings/prefs.ttl'])
+        .toBe(false)
+    })
+  })
+})
